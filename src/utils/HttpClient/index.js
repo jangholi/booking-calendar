8194902f79ba73f09a7767f15fe0d3fcdd8f8e7c
marjan <marjan.jangholi@yahoo.com>
2021-02-28 20:10:33 +0330

    improve httpClient structure

--- src/utils/HttpClient/index.js
+++ src/utils/HttpClient/index.js
@@ -8,7 +8,7 @@ export class HttpClient {
      * set base url
      * @param baseURL {string} base url
      */
-    constructor(baseURL = process.env.REACT_APP_BASE_URL + process.env.REACT_APP_PATH) {
+    constructor(baseURL = process.env.REACT_APP_BASE_URL) {
         this._baseURL = baseURL;
     }
 
@@ -21,7 +21,7 @@ export class HttpClient {
      * @returns {Promise<AxiosResponse<any>>}
      */
     request(method, url, data, params) {
-        let token = 'get token from token management and set it';
+        let token = null;
 
         return Axios.request({
             baseURL: this._baseURL,
